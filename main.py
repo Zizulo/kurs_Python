dfce7a5d3de49849b7e8fa278b8f1c6403f8fc38
Zizulo <danieluzytkownik@gmail.com>
2025-02-22 16:39:41 +0100

    next training from python

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math, time, random, example
+from bp_car import Car
 
 # name = 'Bro Code'
 # print(type(name))
@@ -637,27 +638,33 @@ def withdraw(balance):
         return amount
 
 def main():
-    balance = 0
-    is_running = True
+    # balance = 0
+    # is_running = True
 
-    while is_running:
-        print("Banking Program")
-        print("1. Show Balance")
-        print("2. Deposit")
-        print("3. Withdraw")
-        print("4. Exit")
+    # while is_running:
+    #     print("Banking Program")
+    #     print("1. Show Balance")
+    #     print("2. Deposit")
+    #     print("3. Withdraw")
+    #     print("4. Exit")
 
-        choice = input("Enter your choice (1-4): ")
+    #     choice = input("Enter your choice (1-4): ")
 
-        match choice:
-            case '1': show_balance(balance)
-            case '2': balance += deposit()
-            case '3': balance -= withdraw(balance)
-            case '4': is_running = False
-            case _:
-                print("That is not a valid choice")
+    #     match choice:
+    #         case '1': show_balance(balance)
+    #         case '2': balance += deposit()
+    #         case '3': balance -= withdraw(balance)
+    #         case '4': is_running = False
+    #         case _:
+    #             print("That is not a valid choice")
 
-    print("Thank you, have a nice day")
+    # print("Thank you, have a nice day")
+
+    car1 = Car("Mustang", 2024, "red", False)
+
+    car1.drive()
+    car1.stop()
+    car1.describe()
 
 if __name__ == '__main__':
     main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+def describe(self):
+    #     super().describe()
+    #     print(f"It is a circle with an area of {3.14 * self.radius * self.radius}cm^2")
